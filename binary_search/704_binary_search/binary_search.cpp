0e389ec0b705191717f4d3343ffde7314cb681f4
caijinyang <caijinyang98@gmail.com>
2023-10-09 11:04:44 +0800

    leetcode 704 二分查找

---
+++ binary_search/704_binary_search/binary_search.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gatsby on 2023/10/9.
+//
+
+#include "binary_search.h"
---
+++ binary_search/704_binary_search/binary_search.h
@@ -0,0 +1,33 @@
+//
+// Created by Gatsby on 2023/10/9.
+//
+#include "vector"
+
+#ifndef INC_408_ALGORITHM_BINARY_SEARCH_H
+#define INC_408_ALGORITHM_BINARY_SEARCH_H
+
+
+class binary_search {
+public:
+    int search(std::vector<int> &nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = left + (right - left) / 2;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+};
+
+
+#endif //INC_408_ALGORITHM_BINARY_SEARCH_H
